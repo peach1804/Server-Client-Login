1d13a6a81746dc098af2e4ba4ccd50509d1c6d23
peach1804 <79069554+peach1804@users.noreply.github.com>
2021-06-21 11:43:03 +0800

    Add files via upload

---
+++ ClientLogin/Client.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    private Socket socket;
+    private DataOutputStream outputStream;
+    private DataInputStream inputStream;
+    private String message;
+    private String response;
+    private boolean goNext;
+    private Scanner sc = new Scanner(System.in);
+
+    public void connect() {
+
+        try {
+            socket = new Socket("localhost", 1991);
+            setupStreams(socket);
+            tryLogin();
+            closeAll();
+
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+            System.exit(1);
+        }
+    }
+
+    public void setupStreams(Socket socket) throws IOException {
+        outputStream = new DataOutputStream(socket.getOutputStream());
+        outputStream.flush();
+        inputStream = new DataInputStream(socket.getInputStream());
+    }
+
+    public void closeAll() throws IOException {
+        socket.close();
+        inputStream.close();
+        outputStream.close();
+    }
+
+    public void tryLogin() throws IOException {
+
+        while (true) {
+            message = inputStream.readUTF();
+            System.out.println(message);
+            goNext = inputStream.readBoolean();
+
+            if (goNext) {
+                response = sc.nextLine();
+                outputStream.writeUTF(response);
+                outputStream.flush();
+
+                if (response.equalsIgnoreCase("quit")) {
+                    System.exit(0);
+                }
+            } else {
+                tryLogin();
+            }
+        }
+    }
+}
---
+++ ClientLogin/ClientTest.java
@@ -0,0 +1,8 @@
+public class ClientTest {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connect();
+    }
+
+}
